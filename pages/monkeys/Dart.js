790f5a1c10ae2475e62a4aa3b968e376f1ae06e2
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2021-12-29 02:02:16 -0800

    NavBar5

--- pages/monkeys/Dart.js
+++ pages/monkeys/Dart.js
@@ -1,9 +1,9 @@
 import React from "react";
-class Dart extends React.Component{
+class dart extends React.Component{
     render(){
         return(
             <span>Hello I am a dart Monkey</span>
         )
     }
 }
-export default Dart;
+export default dart;
