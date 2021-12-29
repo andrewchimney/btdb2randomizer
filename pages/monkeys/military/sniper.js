0a1eb074a30a731c02bccac9c63a4443f05d8eb8
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2021-12-29 13:57:28 -0800

    created pages

---
+++ pages/monkeys/military/sniper.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+class sniper extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            imageSize: 100
+        };
+    }
+    render(){
+        return(
+            <div className="monkey">
+                <h1></h1>
+                <p></p>
+                <p>Cost to unlock:  monkey money</p> 
+
+            </div>
+        )
+    }
+}
+export default sniper;
