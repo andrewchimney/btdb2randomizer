d7e19d50851d3eff024a099d015247365bf32f5e
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2021-12-30 16:07:17 -0800

    twitter cards

--- pages/_app.js
+++ pages/_app.js
@@ -11,16 +11,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>BTDB2 Randomizer</title>
         <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="viewport" content='height=device-height, initial-scale=1'/>
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Web site created using create-react-app"/>
+        <meta name="description" content="Bloons Tower Defense Battles 2 Randomizer"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
         <link rel="canonical" href="https://btdb2randomizer.andrewchimney.com"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@nytimes"/>
+        <meta name="twitter:creator" content="@SarahMaslinNir"/>
+        <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral"/>
+        <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."/>
+        <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"></meta>
       </Head>
       <SSRProvider>
         <NavBar/> 
